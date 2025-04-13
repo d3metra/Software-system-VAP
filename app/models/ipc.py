711088a2fab24d0ef6cfd832f300f30de11b8e38
d3metra <HopHay44@gmail.com>
2025-04-13 20:42:44 +0300

    Changed IPC and CPC models. Removed old migration and created one correct

--- app/models/ipc.py
+++ app/models/ipc.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from sqlalchemy import ForeignKey, String
+from sqlalchemy import String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from . import *
@@ -8,7 +8,6 @@ from . import *
 @dataclass
 class IPC(Base):
     __tablename__ = "ipc"
-    ipc_id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, nullable=False)
-    patent_number: Mapped[str] = mapped_column(String(55), ForeignKey(
-        "patents.patent_number", ondelete="CASCADE", onupdate="CASCADE"), nullable=False)
-    ipc_code: Mapped[str] = mapped_column(String(20), nullable=False)
+    ipc_code: Mapped[int] = mapped_column(String(20), primary_key=True, nullable=False)
+    parent_class: Mapped[str] = mapped_column(String(20), nullable=True)
+    title: Mapped[str] = mapped_column(Text, nullable=False)
