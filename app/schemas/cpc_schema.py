8b1334a267c850c56ede2571d98f210bdd21105a
d3metra <HopHay44@gmail.com>
2025-04-14 13:35:59 +0300

    Updated IPC and CPC schemas

--- app/schemas/cpc_schema.py
+++ app/schemas/cpc_schema.py
@@ -4,9 +4,13 @@ from pydantic import BaseModel
 
 class CPC(BaseModel):
     cpc_code: str
+    parent_class: str
+    title: str
 
     @classmethod
     def from_model(cls: type["CPC"], m: models.CPC):
         return cls(
-            cpc_code=m.cpc_code
+            cpc_code=m.cpc_code,
+            parent_class=m.parent_class,
+            title=m.title
         )
--- app/schemas/ipc_schema.py
+++ app/schemas/ipc_schema.py
@@ -4,9 +4,13 @@ from pydantic import BaseModel
 
 class IPC(BaseModel):
     ipc_code: str
+    parent_class: str
+    title: str
 
     @classmethod
     def from_model(cls: type["IPC"], m: models.IPC):
         return cls(
-            ipc_code=m.ipc_code
+            ipc_code=m.ipc_code,
+            parent_class=m.parent_class,
+            title=m.title
         )
