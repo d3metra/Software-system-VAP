f550e8c89ee6d9454a3df5dcc52590769c1b8773
d3metra <HopHay44@gmail.com>
2025-04-14 14:21:13 +0300

    Added the Optional type for parent_class and title fields in IPC and CPC schemas

--- app/schemas/cpc_schema.py
+++ app/schemas/cpc_schema.py
@@ -1,11 +1,13 @@
-from app import models
+from typing import Optional
 
 from pydantic import BaseModel
 
+from app import models
+
 class CPC(BaseModel):
     cpc_code: str
-    parent_class: str
-    title: str
+    parent_class: Optional[str]
+    title: Optional[str]
 
     @classmethod
     def from_model(cls: type["CPC"], m: models.CPC):
--- app/schemas/ipc_schema.py
+++ app/schemas/ipc_schema.py
@@ -1,11 +1,13 @@
-from app import models
+from typing import Optional
 
 from pydantic import BaseModel
 
+from app import models
+
 class IPC(BaseModel):
     ipc_code: str
-    parent_class: str
-    title: str
+    parent_class: Optional[str]
+    title: Optional[str] 
 
     @classmethod
     def from_model(cls: type["IPC"], m: models.IPC):
