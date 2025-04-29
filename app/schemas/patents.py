b757411345f661f3d3c8794d1ce3af5103444392
d3metra <HopHay44@gmail.com>
2025-04-29 19:07:10 +0300

    Updated method in Patents schema

--- app/schemas/patents.py
+++ app/schemas/patents.py
@@ -32,7 +32,7 @@ class PatentResponse(PatentBase):
     cpc_codes: List[cpc_schema.CPC]
 
     @classmethod
-    def from_model(cls: type["Patent"], m: models.Patent, 
+    def from_model(cls: type["Patent"], mP: models.Patent, mF: models.PatentsFamily, 
                    assignees_needed: bool = False, 
                    inventors_needed: bool = False,
                    descriptions_needed: bool = False,
@@ -40,18 +40,19 @@ class PatentResponse(PatentBase):
                    ipc_needed: bool = False,
                    cpc_needed: bool = False):
         return cls(
-            patent_number=m.patent_number,
-            type=m.type,
-            pub_date=m.pub_date,
-            app_date=m.app_date,
-            main_cpc=m.main_cpc,
-            title=m.title,
-            abstract=m.abstract,
-            claims=m.claims,
-            assignees_list=[assignees.Assignee.from_model(assignee) for assignee in m.assignees] if assignees_needed else [],
-            inventors_list=[inventors.Inventor.from_model(inventor) for inventor in m.inventors] if inventors_needed else [],
-            descriptions=[descriptions.Description.from_model(description) for description in m.descriptions] if descriptions_needed else[],
-            citations=[patent_citations.PatentCitation.from_model(citation) for citation in m.citations] if citations_needed else [],
-            ipc_codes=[ipc_schema.IPC.from_model(ipc_code) for ipc_code in m.ipc_codes] if ipc_needed else [],
-            cpc_codes=[cpc_schema.CPC.from_model(cpc_code) for cpc_code in m.cpc_codes] if cpc_needed else []
+            patent_number=mP.patent_number,
+            type=mP.type,
+            pub_date=mP.pub_date,
+            app_number=mF.app_number,
+            app_date=mF.app_date,
+            main_cpc=mF.main_cpc,
+            title=mF.title,
+            abstract=mF.abstract,
+            claims=mF.claims,
+            assignees_list=[assignees.Assignee.from_model(assignee) for assignee in mF.assignees] if assignees_needed else [],
+            inventors_list=[inventors.Inventor.from_model(inventor) for inventor in mF.inventors] if inventors_needed else [],
+            descriptions=[descriptions.Description.from_model(description) for description in mF.descriptions] if descriptions_needed else[],
+            citations=[patent_citations.PatentCitation.from_model(citation) for citation in mP.citations] if citations_needed else [],
+            ipc_codes=[ipc_schema.IPC.from_model(ipc_code) for ipc_code in mF.ipc_codes] if ipc_needed else [],
+            cpc_codes=[cpc_schema.CPC.from_model(cpc_code) for cpc_code in mF.cpc_codes] if cpc_needed else []
         )
